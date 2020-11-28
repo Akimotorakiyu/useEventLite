3beffcd7a38f9d6d848ab17d2793379e41831c74
湫曗 <autumnsilverleaves@gmail.com>
2020-11-29 00:24:40 +0800

    add 嵌套命名空间示例

--- useCase.ts
+++ useCase.ts
@@ -17,11 +17,20 @@ space("eat", () => {
     console.log("drinking!");
   });
 
+  space("drink", () => {
+    // 嵌套命名空间内监听
+    on("smile", () => {
+      console.log("smiling!");
+    });
+
+    // 嵌套命名空间内触发
+    emit("smile"); // 触发事件监听
+  });
+
   // 命名空间内触发
   emit("drink"); // 触发事件监听
 });
 
 emit("drink"); // 无效触发，不在命名空间内
-
 
 
